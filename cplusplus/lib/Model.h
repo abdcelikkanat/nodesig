5f960739876f4663b51aa4485c5d915bfa557b50
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2020-05-24 21:45:02 +0200

    lib

---
+++ cplusplus/lib/Model.h
@@ -0,0 +1,114 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "Graph.h"
+
+using namespace std;
+
+template<typename T>
+class Model {
+private:
+    unsigned int _dim;
+    unsigned int _numOfNodes;
+    float **_weights;
+public:
+
+    Model(unsigned int numTOfNodes, unsigned int dim);
+    ~Model();
+    void encode(vector <Triplet <T>> x, bool *emb);
+    void encodeAll(vector <Triplet <T>> &x, string filePath);
+    random_device _rd;
+
+};
+
+template<typename T>
+void Model<T>::encode(vector <Triplet <T>> x, bool *emb) {
+
+    T dimSum = 0;
+    for(int d=0; d<this->_dim; d++) {
+        dimSum = 0.0;
+        for (int i = 0; i < x.size(); i++)
+            dimSum += x[i].value * this->_weights[x[i].column][d];
+
+        emb[d] = (dimSum > 0) ? true : false;
+
+    }
+
+}
+
+template<typename T>
+void Model<T>::encodeAll(vector <Triplet<T>> &x, string filePath) {
+
+    fstream fs(filePath, fstream::out);
+    if(fs.is_open()) {
+
+        T dimSum;
+        unsigned int idx = 0;
+        bool e;
+
+        fs << _numOfNodes << " " << _dim << endl;
+
+        unsigned int nodePos;
+
+        nodePos = 0;
+        for(unsigned int node = 0; node<_numOfNodes; node++) {
+
+            fs << node << " ";
+            for (int d = 0; d < this->_dim; d++) {
+                idx = nodePos;
+                dimSum = 0;
+                while (x[idx].row == node) {
+                    dimSum += x[idx].value * this->_weights[x[idx].column][d];
+                    idx++;
+                }
+                e = (dimSum > 0) ? 1 : 0;
+                fs << e << " ";
+            }
+            fs << endl;
+            nodePos = idx;
+
+        }
+
+        fs.close();
+
+    } else {
+        cout << "An error occurred during opening the file!" << endl;
+    }
+
+}
+
+template<typename T>
+Model<T>::Model(unsigned int numOfNodes, unsigned int dim) {
+
+    this->_dim = dim;
+    this->_numOfNodes = numOfNodes;
+
+    this->_weights = new T *[numOfNodes];
+    for(int i=0; i < this->_numOfNodes; i++)
+        this->_weights[i] = new T[this->_dim];
+
+    // Sample numbers from normal distribution
+    default_random_engine generator(this->_rd());
+    normal_distribution<T> distribution(0.0, 1.0);
+    for (int i=0; i < this->_numOfNodes; i++) {
+        for (int j=0; j < this->_dim; j++) {
+            this->_weights[i][j] = distribution(generator);
+        }
+    }
+
+}
+
+template<typename T>
+Model<T>::~Model() {
+
+    for(int i=0; i<this->_numOfNodes; i++)
+        delete [] _weights[i];
+    delete [] _weights;
+
+}
+
+#endif //MODEL_H
