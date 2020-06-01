6bdf8aed00ab246cd7c1416aa19effd9ec1e4f11
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2020-06-01 22:27:50 +0200

    deneme

--- cplusplus/src/deneme.cpp
+++ cplusplus/src/deneme.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "deneme.h"
+
+deneme::deneme(int k) {
+
+    cout << "Given value: " << k << endl;
+}
+
+void deneme::carp(Eigen::SparseMatrix<float> &a, Eigen::SparseMatrix<float> &b) {
+
+    //Eigen::SparseMatrix<float> c = a * b;
+    cout <<  a * b << endl;
+
+
+}
